2068bfb3559a6083955a72d6a4ae042295a74201
Nitesh Gautam <niteshgautam@CYG-Admins-MacBook-Pro.local>
2018-02-11 10:52:23 +0530

    signin and register form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import {Route, Switch} from 'react-router-dom';
+import Layout from './hoc/Layout/Layout';
+import Header from '../src/components/Header/Header';
+import Footer from '../src/components/Footer/Footer';
+import Signin from './components/Signin/Signin';
+import Signup from './components/Signup/Signup';
+import Aux from './hoc/Aux/Aux'
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+        <Layout>
+          <Switch>
+            <Route path="/login" component={Signin} />
+            <Route path="/register" component={Signup} />
+          </Switch>
+        </Layout>
       </div>
     );
   }
