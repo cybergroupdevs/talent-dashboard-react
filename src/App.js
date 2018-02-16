b907973fa4c732e28bded66eb588026a13535f83
Nitesh Gautam <niteshgautam@CYG-Admins-MacBook-Pro.local>
2018-02-16 13:52:55 +0530

    add template

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import './App.css';
 import {Route, Switch} from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import Header from '../src/components/Header/Header';
-import Footer from '../src/components/Footer/Footer';
+import EmployeeForm from '../src/components/Form/Form';
 import Signin from './components/Signin/Signin';
+import Employee from './components/Employee/Employee';
 import Signup from './components/Signup/Signup';
+import Dashboard from './components/Dashboard/Dashboard';
 import Aux from './hoc/Aux/Aux'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Layout>
+          <Header/>
           <Switch>
             <Route path="/login" component={Signin} />
             <Route path="/register" component={Signup} />
+            <Route path="/employee_list" component={Employee} />
+            <Route path="/edit" component={EmployeeForm} />
+            <Route path="/" component={Dashboard} />
           </Switch>
         </Layout>
       </div>
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,38 +1,90 @@
 import React, { Component } from 'react';
-import classes from './Signup.css'
-class SignupForm extends Component {
+import axios from 'axios';
+import { Redirect } from 'react-router'
+class Signup extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            "firstName": "",
+            "lastName":"",
+            "emailAddress":"",
+            "employeeCode":"",
+            "password":""
+        };
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+    handleChange = (event) => {
+        this.setState({value: event.target.value});
+    }
+    handleSubmit = () =>{
+        console.log(this.state)
+        // event.preventDefault();
+
+        axios.post('https://talent-dashboard-app.herokuapp.com/register',{responseType: 'json'}, this.state).then((response) => {
+            if(response.data.code == 200){
+
+            }
+            else{
+
+            }
+            console.log(response)
+            this.props.history.push('/login')
+            // if(response)
+            // <Redirect to="/login"/> // Here, nothings happens
+        }).catch((error) => {
+            console.log(error)
+            // const errors = error.response.data.errors ? error.response.data.errors : {};
+            // errors.summary = error.response.data.message;
+
+            // this.setState({
+            //     errors
+            // });
+        });
+    }
     render() {
         return (
-            <div>
-                <form action="">
-                    <div className={classes.imgcontainer}>
-                        <img src="https://media.glassdoor.com/sqll/277861/cyber-group-squarelogo-1404765123554.png" alt="Avatar" className={classes.avatar}/>
+            <div className="container">
+            <div className="row">
+                <div className="col-md-4 col-md-offset-4">
+                    <div className="login-panel panel panel-default">
+                        <div className="panel-div-ui panel-heading ">
+                            <h3 class="panel-title">Please Sign Up</h3>
+                        </div>
+                        <div className="panel-body">
+                        <form role="form">
+                                <fieldset>
+                                    <div className="form-group">
+                                        <input className="form-control" type="text" placeholder="First Name" name="firstName" defaultValue={this.state.firstName} onChange={(event) => this.setState({firstName:event.target.value})} required autofocus/>
+                                    </div>
+                                    <div className="form-group">
+                                        <input className="form-control" type="text" placeholder="Last Name" name="lastName" defaultValue={this.state.lastName} onChange={(event) => this.setState({lastName:event.target.value})} required autofocus/>
+                                    </div>
+                                    <div className="form-group">
+                                        <input className="form-control" type="email" placeholder="Enter Email Address" name="emailAddress" defaultValue={this.state.emailAddress} onChange={(event) => this.setState({emailAddress:event.target.value})} required autofocus/>
+                                    </div>
+                                    <div className="form-group">
+                                        <input className="form-control" type="text" placeholder="Employee Code" name="employeeCode" defaultValue={this.state.employeeCode} onChange={(event) => this.setState({employeeCode:event.target.value})} required autofocus/>
+                                    </div>
+                                    <div className="form-group">
+                                        <input className="form-control" type="password" placeholder="Enter Password" name="password" defaultValue={this.state.password} onChange={(event) => this.setState({password:event.target.value})} required autofocus/>
+                                    </div>
+                                    <div className="checkbox">
+                                        <label>
+                                            <input name="remember" type="checkbox" value="Remember Me"/>Remember Me
+                                        </label>
+                                    </div>
+                                    <button type="button" className="btn btn-lg btn-success btn-block"  onClick={this.handleSubmit}>Register</button>
+                                </fieldset>
+                            </form>
+                        </div>
                     </div>
-                    <div className={classes.container}>
-                        <div>
-                        <input type="email" placeholder="Enter Email Address" name="email_address" required/>
-                        </div>
-                        <div>
-                        <input type="text" placeholder="CGI Code" name="cgi_code" required/>
-                        </div>
-                        <div>
-                        <input type="password" placeholder="Enter Password" name="psw" required/>
-                        </div>
-                        <div>
-                        <button type="submit" className="btn-primary">Register</button>
-                        </div>
-                        <label>
-                        <input type="checkbox" checked="checked" name="remember"/> Remember me
-                        </label>
-                    </div>
-                    <div className={classes.container}>
-                        <button type="button" className={classes.cancelbtn}>Cancel</button>
-                        <div className={classes.psw}>Forgot <a href="#">password?</a></div>
-                    </div>
-                </form>
+                </div>
             </div>
+        </div>
         )
     }
 }
 
-export default SignupForm;
+export default Signup;
